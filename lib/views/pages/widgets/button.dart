8b2e1f9ec45f9e36bad24fcc1db65f3dcb39cd93
GarifullinII <ildaridrisovich@gmail.com>
2023-10-30 11:22:18 +0400

    add button

---
+++ lib/views/pages/widgets/button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../../../utils/constants.dart';
+
+/// Action button
+class Button extends StatelessWidget {
+  /// Constructor
+  /// @param interact - button press action
+  /// @param style - button style
+  /// @param label - button label
+  /// @param child - button child
+
+  const Button({
+    required this.interact,
+    this.style,
+    this.label,
+    this.child,
+    super.key,
+  });
+
+  /// Button action
+  final VoidCallback? interact;
+
+  /// Button style
+  final ButtonStyle? style;
+
+  /// Button label
+  final String? label;
+
+  /// Button child
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+    style: style ?? ElevatedButton.styleFrom(
+      elevation: 1,
+      padding: const EdgeInsets.symmetric(horizontal: 46, vertical: 6),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(36),
+      ),
+      backgroundColor: ColorConstants.inactiveButtonColor,
+    ),
+    onPressed: interact,
+    child: child ??
+        Text(
+          label ?? '',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            color: ColorConstants.inactiveButtonTextColor,
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+  );
+}
